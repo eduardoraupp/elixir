ed2e6b2492c0dc76bd43e141c2237217268df2a6
Eduardo Raupp <rauppduh@gmail.com>
2019-12-15 12:51:53 +0100

    add sequence server

--- lib/sequence/server.ex
+++ lib/sequence/server.ex
@@ -1,17 +1,21 @@
 defmodule Sequence.Server do 
     use GenServer
-
+    alias Sequence.Impl
+  
     def init(initial_number) do
-        { :ok, initial_number }
+      { :ok, initial_number }
     end
-
+  
     def handle_call(:next_number, _from, current_number) do
-        IO.puts "abc"
-        {:reply, current_number, current_number + 1}
+      { :reply, current_number, Impl.next(current_number) }
     end
-
-    def handle_call({:set_number, new_number}, _from, _current_number) do
-        {:reply, new_number, new_number}
+  
+    def handle_cast({:increment_number, delta}, current_number) do
+      { :noreply, Impl.increment(current_number, delta) }
+    end
+  
+    def format_status(_reason, [ _pdict, state ]) do
+      [data: [{'State', "My current state is '#{inspect state}', and I'm happy"}]]
     end
 
 end
